89b424b373d5da3cbacd88088b2700ddd97e3b57
DeepakSR-12 <deepakdeepakeng@gmail.com>
2023-12-02 14:55:45 +0530

    feat: add ui component to fund fusion

---
+++ client/src/components/DisplayCampaigns.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import FundCard from "./FundCard";
+import { loader } from "../assets";
+
+const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (campaign) => {
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign });
+  };
+
+  return (
+    <div>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+        {title} ({campaigns.length})
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 gap-5">
+        {isLoading && (
+          <img
+            src={loader}
+            alt="loader"
+            className="w-[100px] h-[100px] object-contain"
+          />
+        )}
+
+        {!isLoading && campaigns.length === 0 && (
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+            You have not created any campaigns yet
+          </p>
+        )}
+
+        {!isLoading &&
+          campaigns.length > 0 &&
+          campaigns.map((campaign) => (
+            <FundCard
+              key={uuidv4()}
+              {...campaign}
+              handleClick={() => handleNavigate(campaign)}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default DisplayCampaigns;
